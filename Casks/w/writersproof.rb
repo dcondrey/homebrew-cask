3307875fa10468df4e4c326483e12c52cbb1e8ea
David Condrey <davidcondrey@me.com>
2026-07-05 01:05:40 -0700

    writersproof: fix verified, livecheck, style

--- Casks/w/writersproof.rb
+++ Casks/w/writersproof.rb
@@ -2,26 +2,32 @@ cask "writersproof" do
   version "1.5.2"
   sha256 "8815a762d6a221cb852d97c79b5b815db791ced9d88af3e0e54d0a79a61f0684"
 
-  url "https://updates.writerslogic.com/macos/WritersProof-#{version}.dmg"
+  url "https://updates.writerslogic.com/macos/WritersProof-#{version}.dmg",
+      verified: "updates.writerslogic.com/"
   name "WritersProof"
   desc "Cryptographic authorship witnessing for writers and creators"
-  homepage "https://writersproof.com"
+  homepage "https://writersproof.com/"
 
-  depends_on macos: ">= :sonoma"
+  livecheck do
+    url "https://updates.writerslogic.com/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  depends_on macos: :sonoma
 
   app "WritersProof.app"
 
   zap trash: [
-    "~/Library/Application Support/WritersProof",
-    "~/Library/Application Support/CPOP",
-    "~/.writersproof",
-    "~/Library/Caches/com.writerslogic.witnessd",
-    "~/Library/HTTPStorages/com.writerslogic.witnessd",
-    "~/Library/Preferences/com.writerslogic.witnessd.plist",
-    "~/Library/Saved Application State/com.writerslogic.witnessd.savedState",
-  ],
-  rmdir: [
-    "~/Library/Application Support/WritersProof",
-    "~/Library/Application Support/CPOP",
-  ]
+        "~/.writersproof",
+        "~/Library/Application Support/CPOP",
+        "~/Library/Application Support/WritersProof",
+        "~/Library/Caches/com.writerslogic.witnessd",
+        "~/Library/HTTPStorages/com.writerslogic.witnessd",
+        "~/Library/Preferences/com.writerslogic.witnessd.plist",
+        "~/Library/Saved Application State/com.writerslogic.witnessd.savedState",
+      ],
+      rmdir: [
+        "~/Library/Application Support/CPOP",
+        "~/Library/Application Support/WritersProof",
+      ]
 end
